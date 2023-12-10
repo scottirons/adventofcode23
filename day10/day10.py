3eb4a66445bc42e9b6e1e5740e65f73e337e92e6
scottpirons@gmail.com <87613441+scottirons@users.noreply.github.com>
2023-12-10 14:07:53 -0700

    part b day 10 done with some inspiration

--- day10/day10.py
+++ day10/day10.py
@@ -6,9 +6,9 @@ class Solution:
         self.grid = self.parse_input(source)
         self.start = self.find_start()
         self.route = [self.start]
+        self.part_b_grid = [['.' for _ in line] for line in self.grid]
 
-    @staticmethod
-    def parse_input(source):
+    def parse_input(self, source):
         with open(source, "r") as f:
             return f.read().split('\n')
 
@@ -40,11 +40,14 @@ class Solution:
             return -1, 0
 
     def part_a(self):
+        self.part_b_grid[self.start[0]][self.start[1]] = 'J'
         # looking at the chart, it starts here, it's a 7 above it
         curr_loc = [self.start[0] - 1, self.start[1]]
         curr_dir = (0, -1)
+        curr_letter = "7"
 
         while curr_loc != self.start:
+            self.part_b_grid[curr_loc[0]][curr_loc[1]] = curr_letter
             self.route.append(curr_loc)
             curr_loc = [curr_loc[0] + curr_dir[0], curr_loc[1] + curr_dir[1]]
             curr_letter = self.grid[curr_loc[0]][curr_loc[1]]
@@ -52,7 +55,19 @@ class Solution:
 
         return math.ceil(len(self.route) / 2)
 
+    def part_b(self):
+        total = 0
+        for line in self.part_b_grid:
+            parity = 0
+            for char in line:
+                if char == '.' and parity % 2 == 1:
+                    total += 1
+                elif char in "|F7":
+                    parity += 1
+        return total
+
 
 sol = Solution("input.txt")
 print(sol.part_a())
+print(sol.part_b())
 
