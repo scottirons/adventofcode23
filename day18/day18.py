ed31db4368ef210413a42873f78d1244aa025c25
scottpirons@gmail.com <87613441+scottirons@users.noreply.github.com>
2023-12-18 10:00:45 -0700

    part 2 done

--- day18/day18.py
+++ day18/day18.py
@@ -1,4 +1,3 @@
-import re
 import time
 
 
@@ -17,44 +16,42 @@ class Solution:
         for i in ins:
             i = i.split(' ')
             c = i[2][1:-1]
-            self.ins.append((i[0], int(i[1]), c))
+            self.ins.append((i[0], int(i[1])))
             self.ins_b.append((self.dir_key[c[-1]], int(c[1:-1], 16)))
-        print(self.ins_b)
         self.b_a = [(0, 0)]
-        self.dir_a = [self.ins[0][0]]
-        self.i_a = set()
+        self.b_b = [(0, 0)]
         self.dirs = {
-            "R": (0, 1),
-            "L": (0, -1),
-            "U": (-1, 0),
-            "D": (1, 0)
-        }
-        self.shift = {
-            "R": "D",
-            "L": "U",
-            "U": "R",
-            "D": "L"
+            "R": (1, 0),
+            "L": (-1, 0),
+            "U": (0, 1),
+            "D": (0, -1)
         }
 
-    def solve(self):
+    def solve(self, part_b=False):
         # fill out the border
+        source = self.ins_b if part_b else self.ins
+        dest = self.b_b if part_b else self.b_a
+        points = 1
         curr_point = (0, 0)
-        for ins in self.ins:
-            for _ in range(ins[1]):
-                curr_point = (curr_point[0] + self.dirs[ins[0]][0], curr_point[1] + self.dirs[ins[0]][1])
-                self.b_a.append(curr_point)
-                self.dir_a.append(ins[0])
+        for ins in source:
+            curr_point = (curr_point[0] + (self.dirs[ins[0]][0] * ins[1]), curr_point[1] + (self.dirs[ins[0]][1] * ins[1]))
+            points += ins[1]
+            dest.append(curr_point)
 
-        # test is enclosed to the right, not sure about input
-        for i, (x, y) in enumerate(self.b_a):
-            (a, b) = self.dirs[self.shift[self.dir_a[i]]]
-            p = (a + x, b + y)
-            while p not in self.b_a:
-                self.i_a.add(p)
-                p = (p[0] + a, p[1] + b)
-        return len(self.i_a) + len(self.b_a)
+        sum_a = 0
+        sum_b = 0
+        for i in range(len(dest) - 1):
+            sum_a += (dest[i][0] * dest[i + 1][1])
+            sum_b += (dest[i][1] * dest[i + 1][0])
+        total = abs(sum_a - sum_b) / 2
+
+        # ok this was just pure guesswork (since we're already counting them in some capacity I just played around with
+        # how to adjust the points add part until the test input worked
+        return int(total + ((points + 1) // 2))
 
 
 start = time.perf_counter()
-sol = Solution("test.txt")
-print(f"Part A: {sol.solve()}, which took {round(time.perf_counter() - start, 4)} seconds")
+sol = Solution("input.txt")
+print(f'Part 1: {sol.solve()}')
+print(f'Part 2: {sol.solve(True)}')
+print(f'This all took {round(time.perf_counter() - start, 4)} seconds')
