f1081cbcb683045d834884592b35205c0c956124
scottpirons@gmail.com <87613441+scottirons@users.noreply.github.com>
2023-12-21 10:56:46 -0700

    part 1 day 21 done, still working on part 2

---
+++ day21/day21.py
@@ -0,0 +1,70 @@
+DIRS = (
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (-1, 0)
+)
+
+
+def part_1(source):
+    start = (0, 0)
+    with open(source, 'r') as f:
+        lines = f.read().split('\n')
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if lines[i][j] == 'S':
+                start = (i, j)
+                lines[i] = lines[i].replace('S', '.')
+    max_r, max_c = len(lines) - 1, len(lines[0]) - 1
+
+    process = [start]
+    for _ in range(64):
+        visited = set()
+        next_process = []
+        for coord in process:
+            for d in DIRS:
+                next_c = (coord[0] + d[0], coord[1] + d[1])
+                if 0 <= next_c[0] <= max_r and 0 <= next_c[1] <= max_c and next_c not in visited and \
+                        lines[next_c[0]][next_c[1]] == '.':
+                    visited.add(next_c)
+                    next_process.append(next_c)
+        process = next_process
+    print(len(process))
+
+
+def part_2(source):
+    start = (0, 0)
+    with open(source, 'r') as f:
+        lines = f.read().split('\n')
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            if lines[i][j] == 'S':
+                start = (i, j)
+                lines[i] = lines[i].replace('S', '.')
+    max_r, max_c = len(lines), len(lines[0])
+
+    could_work = set()
+    process = [start]
+    for i in range(1, 1000):
+        visited = set()
+        next_process = []
+        for coord in process:
+            for d in DIRS:
+                next_r, next_c = coord[0] + d[0], coord[1] + d[1]
+                next_mod_r, next_mod_c = next_r % len(lines), next_c % len(lines)
+                # i = 132, and could_work is 17153 items long
+                # if i % 2 == 0 and (next_mod_r, next_mod_c) not in could_work:
+                #     print(f'adding a new value to what could work at i = {i}, and could_work is {len(could_work)} '
+                #           f'items long')
+                #     could_work.add((next_mod_r, next_mod_c))
+                if (next_r, next_c) not in visited and lines[next_mod_r][next_mod_c] == '.':
+                    visited.add((next_r, next_c))
+                    next_process.append((next_r, next_c))
+        process = next_process
+    print(len(process))
+
+
+# i = 132, and could_work is 17153 items long
+if __name__ == "__main__":
+    part_1('input.txt')
+    part_2('input.txt')
